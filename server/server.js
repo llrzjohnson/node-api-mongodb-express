f33394de1795889be6a46fce492d9fd09df4d258
L. Lester Johnson <shuai.jan28@gmail.com>
2018-09-09 18:29:08 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -37,6 +38,31 @@ app.get("/todos", (req, res) => {
   );
 });
 
+////
+
+//GET /todos/id23452
+app.get("/todos/:id", (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+    console.log("Nakita error");
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
+/////////
 app.listen(3000, () => {
   console.log("Connected to server - Port 3000");
 });
