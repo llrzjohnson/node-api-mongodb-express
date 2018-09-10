f86fcef18bccb24d474a35c76dfb671613abc786
L. Lester Johnson <shuai.jan28@gmail.com>
2018-09-09 23:18:04 -0400

    Update DB Connecttion

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
-);
+let db = {
+  localhost: "mongodb://localhost:27017/TodoApp",
+  mlab: "mongodb://llrzjohnson:lu1salu1sa@ds251362.mlab.com:51362/todolist"
+};
+mongoose.connect(db.localhost || db.mlab);
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
+// );
 
 module.exports = { mongoose };
