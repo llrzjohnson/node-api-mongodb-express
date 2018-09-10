589da5ce95a7f9abae109742dc6046dfc47664f6
L. Lester Johnson <shuai.jan28@gmail.com>
2018-09-09 23:33:37 -0400

    update DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 let db = {
   localhost: "mongodb://localhost:27017/TodoApp",
-  mlab: "mongodb://llrzjohnson:lu1salu1sa@ds251362.mlab.com:51362/todolist"
+  mlab: "mongodb://lester12345:Lu1salu1sa@ds251362.mlab.com:51362/todolist"
 };
 mongoose.connect(db.localhost || db.mlab);
 
