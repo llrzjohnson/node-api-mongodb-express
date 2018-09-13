89e57774c0669890374351b2fcc03aa19103c184
L. Lester Johnson <shuai.jan28@gmail.com>
2018-09-13 19:07:28 -0400

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -106,6 +106,17 @@ UserSchema.pre("save", function(next) {
   }
 });
 
+UserSchema.methods.removeToken = function(token) {
+  let user = this;
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
+//////////////////////////////////////
+
 let User = mongoose.model("User", UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -163,6 +163,18 @@ app.post("/users/login", (req, res) => {
     });
 });
 
+//DELETE /users
+app.delete("/users/me/token", authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(
+    () => {
+      res.status(200).send();
+    },
+    () => {
+      res.status(400).send();
+    }
+  );
+});
+
 ///////// Server Connection
 app.listen(port, () => {
   console.log(`Connected to port ${port}`);
